22cac809fd1efe7f6e88bcd15d7fc54518a874a0
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-08-07 20:37:05 -0600

    finish all MVP except for adding in Link element. Debugging currently

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route, Link } from 'react-router-dom';
+import MovieList from './Movies/MovieList.js'
+import Movie from './Movies/Movie.js';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/' component={MovieList}/>
+        <Route path='/movies/:id' component={Movie}/>
+      </div>
     </div>
   );
 };
